2a2e52868f2b268b02bb59cfd2d64056f1ec96a7
Derek Chan <djcderek@gmail.com>
2023-08-10 14:26:20 -0700

    Complete exercise 4.2

---
+++ bloglist/app.js
@@ -0,0 +1,18 @@
+//require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+//const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
+const {PORT, MONGODB_URI} = require('./utils/config')
+
+//const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
