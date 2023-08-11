422fd8e797ea30e08962cb02081a588c08c0d429
Derek Chan <djcderek@gmail.com>
2023-08-10 20:39:39 -0700

    Complete exercises 4.6, 4.7

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -7,8 +7,57 @@ const totalLikes = (blogs) => {
         return accumulator + blog.likes
     }, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    const fav = blogs[0]
+    return blogs.reduce((favBlog, currBlog) => {
+        if (favBlog.likes >= currBlog.likes) {
+            return favBlog
+        } else {
+            return currBlog
+        }
+    }, fav)
+}
+
+const mostBlogs = (blogs) => {
+    const authorBlogs =  blogs.reduce((authors, blog) => {
+        if (Object.hasOwn(authors, blog.author)) {
+            authors[blog.author] += 1
+        } else {
+            authors[blog.author] = 1
+        }
+        return authors
+    }, {})
+
+    let blogCount = 0
+    let mostAuthor
+    for (author in authorBlogs) {
+        if (authorBlogs[author] > blogCount) {
+            blogCount = authorBlogs[author]
+            mostAuthor = author
+        }
+    }
+    return {
+        author: mostAuthor,
+        blogs: blogCount
+    }
+}
+
+const mostLikes = (blogs) => {
+     return blogs.reduce((mostLiked, currBlog) => {
+        console.log(mostLiked)
+        if (currBlog.likes >= mostLiked.likes) {
+            mostLiked.likes = currBlog.likes
+            mostLiked.author = currBlog.author
+        }
+        return mostLiked
+    }, {author: "", likes: 0})
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
